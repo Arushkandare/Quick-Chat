0665f705c40c8c5c35254d02264255d3c5e9257b
arushkandare <arushkandare@gmail.com>
2025-06-26 12:22:24 +0530

    Links Commit

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://quick-chat-vite.vercel.app"
     }
 });
 
@@ -25,7 +25,7 @@ initSocket(io);
 
 app.use(express.json({ limit: "4mb" }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://quick-chat-vite.vercel.app",
     credentials: true
 }));
 
